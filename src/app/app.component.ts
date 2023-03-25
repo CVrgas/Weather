5f20fba7e1f66fe0ea8f8bc7a290120bc54b72b6
CVrgas <usual389@gmail.com>
2023-03-25 18:11:45 -0400

    few Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,93 @@
-import { Component } from '@angular/core';
+import { AfterViewChecked, Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewChecked{
+  ngAfterViewChecked(): void {
+    // let container = document.querySelector('.container');
+    // var x = document.getElementById('container')
+  }
+  local_time: Date = new Date()
+  ngOnInit(): void {
+    let container = document.getElementById('container');
+
+    const search = document.getElementById('search-button');
+    const weatherBox = document.getElementById('weather-box');
+    const weatherDetails = document.getElementById('weather-details');
+    const error404 = document.getElementById('not-found');
+
+
+    search?.addEventListener('click', () =>{
+      const APIKey = 'ff435bf1835ffcc9b58c8fb432e6fefc';
+      const city = (<HTMLInputElement>document.getElementById('input')).value;
+
+      if(city == ''){
+        return;
+      }
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        if(json.cod == '404'){
+            container.style.height = '400px';
+            weatherBox.style.display = 'none';
+            weatherDetails.style.display = 'none';
+            error404.style.display = 'block';
+            error404.classList.add('fadeIn');
+            return;
+        }
+        error404.style.display = 'none';
+        error404.classList.remove('fadeIn');
+
+        const image = <HTMLImageElement>document.getElementById('img');
+        const temperature = document.getElementById('temperature');
+        const description = document.getElementById('description');
+        const humidity = document.getElementById('humidity');
+        const wind = document.getElementById('wind');
+        const time = document.getElementById('time');
+
+        switch(json.weather[0].main){
+          case 'Clear':
+            image.src = 'assets/clear.png';
+            break;
+          case 'Snow':
+            image.src = 'assets/snow.png';
+            break;
+          case 'Rain':
+            image.src = 'assets/rain.png';
+            break;
+          case 'Clouds':
+            image.src = 'assets/cloud.png';
+            break;
+          case 'Sun':
+            image.src = 'assets/sun.png';
+            break;
+          case 'Haze':
+            image.src = 'assets/haze.png';
+            break;
+          case 'Mist':
+            image.src = 'assets/haze.png'
+
+        }
+        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°F</span>`;
+        description.innerHTML = `${json.weather[0].description}`;
+        humidity.innerHTML = `${json.main.humidity}%`;
+        wind.innerHTML = `${parseInt(json.wind.speed)}km/h`;
+
+        weatherBox.style.display = '';
+        weatherDetails.style.display ='';
+        weatherBox.classList.add('fadeIn');
+        weatherDetails.classList.add('fadeIn');
+        container.style.height = '550px';
+
+
+      })
+    })
+  }
   title = 'Weather';
+  
 }
+  
